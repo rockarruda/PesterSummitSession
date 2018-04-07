e462a60aebf35275e77f4a6bac6631dc6879a169
Arruda <James.Arruda@fmglobal.com>
2018-04-07 11:35:58 -0400

    Fixed the test

--- New-OctoEnv.Tests.ps1
+++ New-OctoEnv.Tests.ps1
@@ -3,14 +3,39 @@
 
 Describe -Name 'TestOctoEnv' {
 
-    Mock Invoke-Restmethod {
-        result @{
-            Name = "Hey.Doug"
-        }
+    Context -Name 'EnvExits' {
 
+        Mock Get-OctoEnvNames -Verifiable -MockWith {
+        
+            return [PSCustomObject]@{
+                Id = "Environments-99"
+                Name = "Hey.Doug"
+                Description = "TestPost"
+            }
+    
+        }
+         
+        It 'Should return environment exists' {
+            $EnvName = New-OctoEnv -OctoEnvName "Hey.Doug"
+            $EnvName | Should -Be "Hey.Doug already exists"
+        }
     }
 
-    Context -Name 'EnvExits' {
+    Context -Name 'Env does not exist, create it'{
+
+        Mock Invoke-RestMethod -Verifiable -MockWith {
         
+            return [PSCustomObject]@{
+                Id = "Environments-99"
+                Name = "Hey.Doug"
+                Description = "TestPost"
+            }
+    
+        }
+
+        It 'Should Create Hey.Doug' {
+            $CreateEnv = New-OctoEnv -OctoEnvName "Hey.Doug"
+            $CreateEnv.Name | Should -Be 'Hey.Doug'
+        }
     }
 }
