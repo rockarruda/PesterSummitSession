1ad467bb8b1abc9985419ab8e3ecb642f9564bd7
Arruda <James.Arruda@fmglobal.com>
2018-04-10 10:35:58 -0400

    Updated Overview

--- PesterOverview.ps1
+++ PesterOverview.ps1
@@ -3,13 +3,13 @@ I call this the foundation block, where you can load modules or scripts into mem
 your tests.#>
 . .\Get-Token.ps1
 
-Describe -Name 'TestToken' -Tag Bits{ #This is the describe block which names the test, and you can also set a tag ex.. Unit or integrated.
+Describe -Name 'TestToken' -Tag 'Unit'{ #This is the describe block which contains a group of tests, and you can also set a tag for ex.. Unit or integrated.
     #Testing the function Get-Token
-    Context -Name 'Get-Token' { #This block is used to group a set of tests in the decribe block, use case would be scenarios etc..
-        Mock Get-Token {return @{Name = "tokenbroker"}}
-        It "Should Return Token"{ #The It block is the test itself
+    Context -Name 'Get-Token' { #This block is used to group a set of tests (It blocks) in the decribe block, use case would be scenarios etc..
+        
+        It "Should Return Token"{ #The It block is the test itself and validates the assertion
             $Token = Get-Token
-            $Token.name | Should <#Command used to compare objects#> be 'tokenbroker'
+            $Token.name | Should <#Command used to compare objects to be true of false#> be 'tokenbroker'
         }
     }
 }
