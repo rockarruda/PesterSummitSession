053373a6735a785ff3ad1d947c1498559dca6122
Arruda <James.Arruda@fmglobal.com>
2018-04-11 10:52:29 -0400

    Updated Comments

--- PesterBasics/Get-Token.Tests.ps1
+++ PesterBasics/Get-Token.Tests.ps1
@@ -5,6 +5,8 @@
     Describe -Name 'TestToken' -Tag Bits{
         #Testing the function Get-Token
         Context -Name 'Get-Token' {
+            #Mock line is commented out to show code coverage, once uncommented code coverage of my function`
+            #will be 0% proving the mock is working.
             #Mock Get-Token {return @{Name = "tokenbroker"}}
             It "Should Return TokenBroker"{
                 $Token = Get-Token
